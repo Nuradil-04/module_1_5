cafe699f5ec3cf5983d2ff38738c03679c591947
Nuradil-04 <hirurgiya.prof@gmail.com>
2024-11-30 21:37:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string=('Верь в меня,верь в мою веру в себя')
+simvols = len(my_string)
+print("Количество символов:", simvols)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
